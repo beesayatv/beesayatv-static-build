76ea252fd30fa40ea2e09f65540d13ae26fbce2d
cocoholo <beesayatv@gmail.com>
2026-07-24 09:22:46 +0800

    ver

--- dist/wp-content/themes/bloggerly-child/js/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.btv-site-header__search, .btv-site-header__atlas').forEach(function (headerAction) {
-        headerAction.addEventListener('click', function (event) {
-            if (event.detail > 0) {
-                headerAction.blur();
-            }
-        });
-    });
-});
